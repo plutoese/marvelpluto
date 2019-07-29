99f0a6d20c18e130c579e9647cd52bb7fcc64b19
plutoese <glen.zhang7@gmail.com>
2019-07-29 10:59:52 +0800

    update 20190729 version 0.1

---
+++ marvelpluto/application/proxypool/proxy_pool-master/Config/setting.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name：     setting.py
+   Description :   配置文件
+   Author :        JHao
+   date：          2019/2/15
+-------------------------------------------------
+   Change Activity:
+                   2019/2/15:
+-------------------------------------------------
+"""
+
+# database config
+from os import getenv
+
+
+class ConfigError(BaseException):
+    pass
+
+
+DB_TYPE = getenv('db_type', 'SSDB')
+
+if DB_TYPE == 'SSDB':
+    DB_HOST = getenv('ssdb_host', '127.0.0.1')
+    DB_PORT = getenv('ssdb_port', '6379')
+    DB_PASSWORD = getenv('ssdb_password', '')
+elif DB_TYPE == 'MONGODB':
+    DB_HOST = getenv('mongodb_host', '127.0.0.1')
+    DB_PORT = getenv('mongodb_host', '27017')
+    DB_PASSWORD = getenv('mongodb_password', '')
+else:
+    raise ConfigError('Unknown database type, your environment variable `db_type` should be one of SSDB/MONGODB.')
+
+DATABASES = {
+    "default": {
+        "TYPE": "MONGODB",  # TYPE SSDB/MONGODB if use redis, only modify the host port, the type should be SSDB
+        "HOST": "47.103.84.137",
+        "PORT": 27017,
+        "NAME": "proxypool",
+        "PASSWORD": "z1Yh2900"
+
+    }
+}
+
+# register the proxy getter function
+
+PROXY_GETTER = [
+    "freeProxy01",
+    "freeProxy02",
+    "freeProxy03",
+    "freeProxy04",
+    "freeProxy05",
+    "freeProxy06",
+    "freeProxy07",
+    "freeProxy08",
+    "freeProxy09",
+]
+
+# # API config http://127.0.0.1:5010
+
+SERVER_API = {
+    "HOST": "0.0.0.0",  # The ip specified which starting the web API
+    "PORT": 5010  # port number to which the server listens to
+}
